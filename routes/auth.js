f8a55c698f985444da5d220bc2197502d1c3d3e7
Blues Henderson <bluesh322@gmail.com>
2021-03-09 14:25:43 -0600

    Finished user.js and tested

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,27 @@
+const express = require("express");
+
+
+const User = require("../models/user");
+const Message = require("../models/message");
+const { BCRYPT_WORK_FACTOR } = require("../config");
+const ExpressError = require("../expressError");
+
+const router = new express.Router();
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
  *
  **/
+
+router.post("/login", async function(req, res, next) {
+    try {
+      const customers = await User.login();
+      return res.render("customer_list.html", { customers });
+    } catch (err) {
+      return next(err);
+    }
+  });
 
 
 /** POST /register - register user: registers, logs in, and returns token.
@@ -11,3 +30,16 @@
  *
  *  Make sure to update their last-login!
  */
+
+router.post("/register", async function(req, res, next) {
+    try {
+      const user = await User.register(username, password, first_name, last_name, phone);
+      if (!User.authenticate(user.username, user.password)) {
+          throw new ExpressError("Invalid username or password", 404);
+      }
+      User.updateLoginTimestamp(user.username);
+      return res.render("index.html", { user });
+    } catch (err) {
+      return next(err);
+    }
+  });
